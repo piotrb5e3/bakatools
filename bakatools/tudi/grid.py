b4daf3d2428b72a07c92a06f46482163ad29d951
Piotr Bakalarski <piotr.bakalarski@allegro.com>
2023-12-26 00:08:02 +0100

    Add algorithm for checking visibility from edges on a 2D grid

--- bakatools/tudi/grid.py
+++ bakatools/tudi/grid.py
@@ -28,7 +28,7 @@ class Grid2D(dict[Vector2D, T]):
         return result
 
     def to_graph(
-        self, can_go_from_to: Callable[[str, Vector2D, str], bool]
+            self, can_go_from_to: Callable[[T, Vector2D, T], bool]
     ) -> DirectedGraph[Vector2D]:
         result: dict[Vector2D, dict[Vector2D, int]] = defaultdict(lambda: dict())
 
@@ -36,8 +36,57 @@ class Grid2D(dict[Vector2D, T]):
             for direction in direction_vectors:
                 new_pos = start_pos.add(direction)
                 if new_pos in self and can_go_from_to(
-                    self[start_pos], direction, self[new_pos]
+                        self[start_pos], direction, self[new_pos]
                 ):
                     result[start_pos][new_pos] = 1
 
         return DirectedGraph[Vector2D](result)
+
+
+T2 = TypeVar("T2")
+
+
+def transform_values(grid: Grid2D[T], f: Callable[[T], T2]) -> Grid2D[T2]:
+    result = Grid2D[T2]({k: f(v) for k, v in grid.items()})
+    result.x_span = grid.x_span
+    result.y_span = grid.y_span
+    return result
+
+
+def visible_points(grid: Grid2D[int]) -> set[Vector2D]:
+    visible = set[Vector2D]()
+
+    for x in range(grid.x_span):
+        top = -1
+        for y in range(grid.y_span):
+            tree = Vector2D(x, y)
+            h = int(grid[tree])
+            if grid[tree] > top:
+                visible.add(tree)
+                top = grid[tree]
+
+    for x in range(grid.x_span):
+        top = -1
+        for y in reversed(range(grid.y_span)):
+            tree = Vector2D(x, y)
+            h = int(grid[tree])
+            if grid[tree] > top:
+                visible.add(tree)
+                top = grid[tree]
+
+    for y in range(grid.y_span):
+        top = -1
+        for x in range(grid.x_span):
+            tree = Vector2D(x, y)
+            if grid[tree] > top:
+                visible.add(tree)
+                top = grid[tree]
+
+    for y in range(grid.y_span):
+        top = -1
+        for x in reversed(range(grid.x_span)):
+            tree = Vector2D(x, y)
+            if grid[tree] > top:
+                visible.add(tree)
+                top = grid[tree]
+    return visible
