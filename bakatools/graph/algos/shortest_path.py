e57440fc923fc8829d7d632e4d0eef6029ecbb74
Piotr Bakalarski <piotr.bakalarski@allegro.com>
2023-12-31 15:35:31 +0100

    Add floyd-warshall algorithm for finding the shortest path between any two nodes in graph

--- bakatools/graph/algos/shortest_path.py
+++ bakatools/graph/algos/shortest_path.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from queue import PriorityQueue
 from typing import TypeVar
 
@@ -21,3 +22,25 @@ def shortest_paths(graph: DirectedGraph[T], start: T) -> dict[T, int]:
                 pq.put((distance + next_dist, next_node))
 
     return shortest_paths_dict
+
+
+def floyd_warshall(graph: DirectedGraph[T]) -> dict[T, dict[T, int | None]]:
+    """Implements the floyd-warshall algorithm for finding the shortest paths in a graph between any pair of nodes"""
+    result: dict[T, dict[T, int | None]] = defaultdict(lambda: defaultdict(lambda: None))
+    for node, node_values in graph.items():
+        result[node][node] = 0
+        for dest_node, weight in node_values.items():
+            result[node][dest_node] = weight
+
+    for intermediate_node in graph.keys():
+        for start_node in graph.keys():
+            for end_node in graph.keys():
+                old = result[start_node][end_node]
+                p1 = result[start_node][intermediate_node]
+                p2 = result[intermediate_node][end_node]
+                if p1 is not None and p2 is not None:
+                    if old is None:
+                        result[start_node][end_node] = p1 + p2
+                    else:
+                        result[start_node][end_node] = min(result[start_node][end_node], p1 + p2)
+    return result
